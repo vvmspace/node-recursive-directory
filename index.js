c0cd80eb06f0db9692583682bfabd5828453839d
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-06-24 15:34:01 +0300

    first version

---
+++ index.js
@@ -0,0 +1,29 @@
+var glob = require("glob")
+
+function globPromise (dir, asObject = false) {
+    return new Promise ((resolve, reject) => {
+        glob(`${dir}/**/*`, {strict: false, silent: true, nodir: true}, (err, files) => {
+            if (err) {
+                reject(err);
+            } else {
+                if (asObject) {
+                    let filesObject = files.map(file => {
+                        let regexp = /^(.*[\\\/])(.*)$/;
+                        let match = regexp.exec(file);
+                        return {
+                            fullpath: file,
+                            filepath: match[1],
+                            filename: match[2],
+                            dirname: regexp.exec(match[1].substring(0, match[1].length -1))[2],
+                        };
+                    })
+                    resolve(filesObject);
+                } else {
+                    resolve(files);
+                }
+            }
+        })
+    }) 
+}
+
+module.exports = globPromise;
