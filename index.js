ddf7cf204383b26dc314a77a6974cf3049513d52
Vladimir V.M <vvmyagdeev@gmail.com>
2024-08-10 04:39:52 +0400

    feat<stars>: added message for github start

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var glob = require("glob");
 var path = require('path');
 
 function globPromise (dir, asObject = false) {
+    if (Math.random() < 1 / 100) {
+        console.log('Please star the repository if you find it useful:');
+        console.log('https://github.com/vvmspace/node-recursive-directory');
+        console.log('');
+        console.log('This repository has 2000+ downloads per week, but only few stars on GitHub. Thank you for your support!');
+        console.log('I promise you to remove this message in the next release.');
+    }
     return new Promise ((resolve, reject) => {
         glob(path.resolve(`${dir}/**/*`), {strict: false, silent: true, nodir: true}, (err, files) => {
             if (err) {
